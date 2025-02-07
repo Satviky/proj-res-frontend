19f5cb71de9a21bb708b00f5a7cc3b625fc9ccc5
Satviky <126392608+Satviky@users.noreply.github.com>
2025-02-07 15:49:20 +0530

    revert

--- src/components/ChatUI.js
+++ src/components/ChatUI.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './styles.css';
+// import './ChatUI.css';
 import Messages from './Messages';
 import Options from './Options';
 
@@ -48,27 +48,23 @@ const ChatUI = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen bg-gray-900 text-white">
-      <div className="flex-grow overflow-y-auto p-4">
+    <div className="chat-container">
+      <div className="messages">
         {messages.map((message, index) => (
-          <div key={index} className={`my-2 p-3 max-w-xs rounded-lg ${message.sender === 'user' ? 'bg-blue-500 text-white self-end' : 'bg-gray-700 text-white self-start'}`}>
+          <div key={index} className={`message ${message.sender === 'user' ? 'sent' : 'received'}`}>
             {message.text}
+            <div className="timestamp">{/* Add timestamp here if needed */}</div>
           </div>
         ))}
       </div>
-      <div className="p-4 border-t border-gray-800 bg-gray-800">
-        <div className="flex">
-          <input 
-            type="text" 
-            value={input} 
-            onChange={handleInputChange} 
-            className="flex-grow p-2 rounded-l-lg bg-gray-700 border border-gray-600 focus:outline-none focus:border-blue-500"
-            placeholder="Type a message"
-          />
-          <button onClick={handleSendMessage} className="bg-blue-500 text-white px-4 py-2 rounded-r-lg">
-            Send
-          </button>
-        </div>
+      <div className="input-box">
+        <input 
+          type="text" 
+          value={input} 
+          onChange={handleInputChange} 
+          placeholder="Type a message" 
+        />
+        <button onClick={handleSendMessage}>Send</button>
       </div>
     </div>
   );
